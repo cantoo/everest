30388650d937a236723506a00987bfe50bdda070
root <cantoo523>
2018-11-13 14:10:47 +0800

    remove pcall

--- resty/everest.lua
+++ resty/everest.lua
@@ -1,3 +1,5 @@
+local restful = require("resty.restful")
+
 local table_concat = table.concat
 local ngx_exit = ngx.exit
 local ngx_var = ngx.var
@@ -5,14 +7,8 @@ local ngx_var = ngx.var
 local res1 = ngx_var[1]
 local echo_request_method = string.lower(ngx_var.echo_request_method)
 local method_resources_file = table_concat({echo_request_method, res1, ngx_var[3], ngx_var[5]}, "_")
-
 local res1_sub = ngx.re.match(res1, ".*_(.*)$")
 res1_sub = res1_sub and res1_sub[1] or res1
-local success, handler = pcall(require, table_concat({res1_sub, method_resources_file}, "."))
---local success, handler = true, require(table_concat({res1_sub, method_resources_file}, "."))
-if not success or not handler or type(handler.handle) ~= "function" then
-    ngx_exit(ngx.HTTP_NOT_FOUND)
-end
 
-local restful = require("resty.restful")
+local handler = require(table_concat({res1_sub, method_resources_file}, "."))
 restful.say(handler.handle())
